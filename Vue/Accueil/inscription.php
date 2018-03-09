e075694ed06baa41ae1566ed10d1d5e78ce9be31
Etheram68 <francois681927@hotmail.fr>
2018-03-09 20:28:52 +0100

    Ajout des champs obligatoire plus champs email

--- Vue/Accueil/inscription.php
+++ Vue/Accueil/inscription.php
@@ -11,37 +11,37 @@ $secret = '6LcWfj4UAAAAAEgMUSRlSX8HgYzwTgk6rHkMTrzp'; // votre clé privée
                 <div class="col-sm-12 form-group">
                     <label>Nom</label>
                     <input name="nom" type="text" placeholder="Entrez votre nom"
-                           class="form-control">
+                           class="form-control" required>
                 </div>
                 <div class="col-sm-12 form-group">
                     <label>Prénom</label>
                     <input name="prenom" type="text" placeholder="Entrez votre prénom"
-                           class="form-control">
+                           class="form-control" required>
                 </div>
                 <div class="col-sm-12 form-group">
                     <label>Date de naissance (AAAA-MM-JJ)</label>
                     <input type="text" name="dateNaissance"
-                           placeholder="Entrez votre date de naissance.." class="form-control">
+                           placeholder="Entrez votre date de naissance.." class="form-control" required>
                 </div>
                 <div class="col-sm-12 form-group">
                     <label>Email</label>
-                    <input type="text" name="email" placeholder="Entrez votre email.."
-                           class="form-control">
+                    <input type="email" name="email" placeholder="Entrez votre email.."
+                           class="form-control" required>
                 </div>
                 <div class="col-sm-12 form-group">
                     <label>Login</label>
                     <input name="pseudo" type="text" placeholder="Entrez un identifiant.."
-                           class="form-control">
+                           class="form-control" required>
                 </div>
                 <div class="col-sm-6 form-group">
                     <label>Mot de passe</label>
                     <input type="password" name="mdp" placeholder="Entre votre mot de passe.."
-                           class="form-control">
+                           class="form-control" required>
                 </div>
                 <div class="col-sm-6 form-group">
                     <label>Vérification du mot de passe</label>
                     <input type="password" name="verif_mdp"
-                           placeholder="Retapez votre mot de passe.." class="form-control">
+                           placeholder="Retapez votre mot de passe.." class="form-control" required>
                 </div>
                 <div class="col-sm-6 col-lg-offset-2 form-group">
                     <div class="g-recaptcha" data-sitekey="<?php echo $siteKey; ?>">
