be829d4dd3fe7fef04019971ba2ae6bf8ad57057
Etheram68 <francois681927@hotmail.fr>
2018-03-13 17:25:53 +0100

    Ajout et modification de la page de session

--- Vue/Accueil/splash.php
+++ Vue/Accueil/splash.php
@@ -15,7 +15,7 @@
                 Ce site démo a été développé avec Bootstrap 3.3.7<br>
 
                 <br>
-                Il est disponible en OpenSource sur <a href="#">dépot GIT <img src="Contenu/images/symbol/gitlogo.png" alt="Logo Git"></a><br>
+                Il est disponible en OpenSource sur <a href="https://github.com/Etheram68/Blog">dépot GIT <img src="Contenu/images/symbol/gitlogo.png" alt="Logo Git"></a><br>
                 <a href="#">Documentation du site</a>
                 <br>
                 Pour tester le site vous pouvez vous identifier sur l'interface admin :<br>
@@ -23,7 +23,8 @@
                 Accés administraeur - Login: admin - Pass: admin<br>
                 Accés modérateur - Login: modo - Pass: modo<br>
                 Accés utilisateur - Login: user - Pass: user<br>
-                <br>                              
+                <br>
+                Le livre Croc Blanc à été utilisé à des fins estétique pour l'acheter cliquez <a heref="https://www.amazon.fr/Croc-Blanc-Jack-London/dp/2092506927"> ici  </a>                            
                 <p>
                     <a class="btn btn-info" href="">Ok, C'est parti !</a>
                 </p>
